3675a37c0abe2fb24420b211d53304c37eab12b0
nguyet33 <tdnguyet33@gmail.com>
2023-03-30 15:41:06 -0700

    var declaration

---
+++ script.js
@@ -0,0 +1,27 @@
+// Assignment code here
+var characterLength = 8;
+var choiceArr = [];
+
+var upperCaseArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var lowerCaseArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var numberCaseArr = ['0','1','2','3','4','5','6','7','8','9'];
+var specialCharacterArr =['!','@','#','$%','^','&','*','(',')','-','_','=','+','/'];
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+function generatePassword(){
+
+}
